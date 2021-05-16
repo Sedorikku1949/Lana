a5513ae3803dd1fb76926e657dda66b8af97a73d
Sedorriku <73949405+Sedorikku1949@users.noreply.github.com>
2021-05-16 11:36:05 +0200

    managers

---
+++ managers/commandsManager.js
@@ -0,0 +1,55 @@
+﻿const chalk = require("chalk")
+const { client } = require("../managers/eventsManagers")
+const fs = require("fs")
+
+const ignoreCategorie = ["moderation", "dev", "gif"]
+const ignoreChannel = [ "782235571094945794" , "836515060124745769", "782235571761315848"]
+const blacklistFolder = ["subWarnings", "subGuilds"]
+
+function getArgs(message, prefixes) {
+    if (!prefixes || typeof prefixes !== "object") throw new Error("Invalid Prefix Value");
+    const content = message.content.toLowerCase();
+    for (const prefix_ of prefixes)
+        if (content.startsWith(prefix_)) {
+            var PREFIX = content.slice(0, prefix_.length);
+            var command = content.slice(prefix_.length).trim().split(/\s+/g)[0];
+            var args = content.slice(prefix_.length + command.length + 1).trim().split(/\s+/g);
+        }
+    return { prefix: PREFIX, command: command, args: args };
+};
+
+const errorLog = []
+
+module.exports = {
+    init: () => {
+        const cmd = []
+        function getFiles(dir) {
+            fs.readdirSync(dir).forEach((subdir) => {
+                if (subdir.match(/\.js/g) && !subdir.match(/\.json/g)) try { cmd.push(require(`../${dir}/${subdir}`)); } catch(err) { console.log(chalk.red(`{ ERROR }  >>  An error as occured when loading "${dir}/${subdir}"`)); errorLog.push({file: `${dir}/${subdir}`, error: err}) }
+                else if (subdir.match(/\./g) || blacklistFolder.includes(subdir) || subdir.startsWith("sub")) return
+                else getFiles(`${dir}/${subdir}`)
+            })
+        }
+        getFiles("cmd")
+        console.log(`${cmd.length} commands has been loaded`)
+        console.log(chalk.red(`Résumé des erreurs :\n${errorLog.length > 0 ? errorLog.map(e => `{ file: ${e.file}, error: ${e.error} }`).join('\n\n') : "Aucune erreur"}`))
+        return cmd
+    },
+    execute: (message) => {
+        const { prefix, command, args } = getArgs(message, client.prefix)
+        if (!prefix || !command) return
+        
+        const commandToExport = client.commands.find(e => { if (e.config.name == command || e.config.aliases.length > 0 && e.config.aliases.includes(command)) return e })
+        if (!commandToExport) return
+
+        if (message.guild.id !== "796376442866368513" && commandToExport.config.handler.staff && !message.member.roles.cache.has("782235568012001285")) return message.channel.send({ embed: { color: '#FF6565', description: "> **⛔ Cette commande est réservée aux modérateurs !**" } })
+        if (message.guild.id !== "796376442866368513" && commandToExport.config.handler.dev && !message.member.roles.cache.has("782235568012001288")) return message.channel.send({ embed: { color: '#FF6565', description: "> **⛔ Cette commande est réservée aux développeurs !**" } })
+        if (message.guild.id !== "796376442866368513" && commandToExport.config.handler.inPogress && !message.member.roles.cache.has("782235568012001285") && message.channel.id !== "836515060124745769") return message.channel.send({ embed: { color: '#FF6565', description: "> **⛔ Cette commande est encore en développement !**" } })
+        if (message.guild.id !== "796376442866368513" && !message.member.roles.cache.has("782235568012001285") && !ignoreCategorie.includes(commandToExport.config.category) && !ignoreChannel.includes(message.channel.id)) return
+        
+        try { commandToExport.exe(message, args, [], prefix, command) ; if (commandToExport.config.handler.deleteInvoke) message.delete().catch(() => false) }
+            catch(err) { console.log(chalk.red(`{ ERROR }  >>  An error as occured when executing the command "${commandToExport.config.name}" :\n${err}`)) }
+
+        console.log(`{ COMMAND EXECUTOR } ${chalk.yellow(`< ${getDate(Date.now(), `[DD]/[MM]/[YYYY] à [hh]:[mm] et [ss]:[ms]`)} | ${Date.now()} >`)} command "${commandToExport.config.name}" executed by ${chalk.cyan(`${message.author.tag} / ${message.author.id}`)} in ${chalk.blue(`( #${message.channel.name} | ${message.channel.id} )`)}`)
+    }
+}
---
+++ managers/eventsManagers.js
@@ -0,0 +1,34 @@
+﻿const start = Date.now()
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const fs = require("fs")
+const config = require("../config.json")
+const chalk = require("chalk")
+
+module.exports = {
+    client: client,
+    start: start,
+}
+
+client.prefix = config.prefix
+let error = []
+
+fs.readdirSync("listeners").forEach((dir) => {
+    if (dir.match(/\./g)) return console.log(chalk.yellow(`{ WARNING }  >>  The file "listeners/${dir}" has been ignored by the event loader`))
+    if (!fs.existsSync(`listeners/${dir}/exe.js`)) return console.log(chalk.red(`{ ERROR }  >>  The folder "listeners/${dir}" doesn't have a file named "exe.js"`))
+    try { client.on(dir, require(`../listeners/${dir}/exe.js`)); console.log(chalk.blue(`{ EVENTS }  >> The event "listeners/${dir}" loaded successfully at "listeners/${dir}/exe.js"`)) } catch(err) { console.log(chalk.red(`{ EVENT ERROR }  >>  An error as occured when loading "listeners/${dir}/exe.js""`)); error.push({file: `dir/exe.js`, error: err}) }
+})
+
+if (error.length > 0) {
+    console.log(chalk.red(`Résumé des erreurs :\n${error.map(e => require("util").inspect(e)).join("\n")}`))
+}
+
+client.data = new Map(); client.queue = []; client.music = {}
+fs.readdirSync("data").forEach(async (dir) => client.data.set(dir.replace(".json", ""), require(`../data/${dir}`)))
+client.stats = { global: {}, user: {}, channel: {}, join: {}, leave: {}, vocal: {}}
+require("../functions/proto/prototype")
+require("../functions/load")()
+client.commands = require("./commandsManager").init()
+client.ia = {}
+
+client.login(config.token)
