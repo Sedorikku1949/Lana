c7fd16ea93ba194646150c7fdd9b13e4a404e95d
Sedorriku <73949405+Sedorikku1949@users.noreply.github.com>
2021-05-16 11:51:13 +0200

    fail

--- xp/gestion.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    exe: async (message, args, modules, prefix, command) => {},
-    config: {
-        name: "gestion", aliases: [], category: "moderation", handler: { deleteInvoke: true, staff: true, dev: false, inPogress: true }, help: { desc: "...", use: "..." }, modules: ["fs"]
-    }
-  };
-
-  // =xp add/remove user xp
